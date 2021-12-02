d609b3987ba5a1d5b84fbbb0a1117d9a1dc1f7e1
Kush Mishra <kush.mishra@philips.com>
2021-12-02 21:26:02 +0530

    Added router

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.render('index')
 })
+
 
 app.listen(5000)
