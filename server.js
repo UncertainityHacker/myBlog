946ffa6b40ab730c0e3d454217c900802afaa5ce
Kush Mishra <kush.mishra@philips.com>
2021-12-03 20:43:16 +0530

    pages added

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const articleRouter = require('./routes/articles')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1/myBlog')
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended:false }))
 
 app.get('/', (req,res) => {
-    res.render('index')
+    const articles = [{
+        title:'First Article',
+        createdAt: new Date(),
+        description: 'First Description'
+    },{
+        title:'Second Article',
+        createdAt: new Date(),
+        description: 'Second Description'
+    }]
+    res.render('articles/index', { articles: articles })
 })
 
+app.use('/articles', articleRouter)
 
 app.listen(5000)
